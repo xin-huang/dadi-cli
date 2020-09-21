1a986b624de597de924040bd2edf9e06aa3080ba
Xin Huang <xin-huang@users.noreply.github.com>
2020-09-20 19:11:53 -0700

    20200920 Update codes

--- src/Plot.py
+++ src/Plot.py
@@ -1,34 +1,50 @@
-import dadi
+import dadi, pickle
+import numpy as np
 import matplotlib.pyplot as plt
  
 fig = plt.figure(figsize=(8,6))
 
 def plot_single_sfs(fs, projections, output):
-    if projections != None:
-        fs = fs.project(projections)
+
+    fs = dadi.Spectrum.from_file(fs)    
+    
+    fig = plt.figure(219033)
     if len(fs.sample_sizes) == 1:
+        if projections == None: projections = [20]
+        fs = fs.project(projections)
         dadi.Plotting.plot_1d_sfs(fs)
     if len(fs.sample_sizes) == 2:
+        if projections == None: projections = [20, 20]
+        fs = fs.project(projections)
         dadi.Plotting.plot_single_2d_sfs(fs)
     fig.savefig(output)
 
 def plot_comparison(fs, fs2, projections, output, vmin, resid_range):
-    if projections != None:
+
+    fs = dadi.Spectrum.from_file(fs)
+    fs2 = dadi.Spectrum.from_file(fs2)
+
+    fig = plt.figure(219033)
+    if len(fs.sample_sizes) == 1:
+        if projections == None: projections = [20]
         fs = fs.project(projections)
         fs2 = fs2.project(projections)
-    if len(fs.sample_sizes) == 1:
         dadi.Plotting.plot_1d_comp_Poisson(model=fs, data=fs2)
     if len(fs.sample_sizes) == 2:
+        if projections == None: projections = [20, 20]
+        fs = fs.project(projections)
+        fs2 = fs2.project(projections) 
         dadi.Plotting.plot_2d_comp_Poisson(model=fs, data=fs2, vmin=vmin, resid_range=resid_range)
     fig.savefig(output)
 
-def plot_fitted_demography(fs, model, demography_params, projections, misid, output, vmin, resid_range):
+def plot_fitted_demography(fs, model, popt, projections, misid, output, vmin, resid_range):
     
     from Models import get_dadi_model_func
     func = get_dadi_model_func(model)
 
-    if projections != None:
-        fs = fs.project(projections)
+    popt = np.array(open(popt, 'r').readline().rstrip().split()[1:-1], dtype=float)
+
+    fs = dadi.Spectrum.from_file(fs)
     if misid:
         func = dadi.Numerics.make_anc_state_misid_func(func)
     func_ex = dadi.Numerics.make_extrap_func(func)
@@ -36,20 +52,58 @@ def plot_fitted_demography(fs, model, demography_params, projections, misid, out
     pts_l = [int(ns[0]+10), int(ns[0]+20), int(ns[0]+30)]
 
     model = func_ex(popt, ns, pts_l)    
-    # Likelihood of the data given the model AFS.
-    ll_model = dadi.Inference.ll_multinom(model, fs)
-    print('Maximum log composite likelihood: {0}'.format(ll_model))
-    # The optimal value of theta given the model.
-    theta = dadi.Inference.optimal_sfs_scaling(model, fs)
-    print('Optimal value of theta: {0}'.format(theta))
-    
+   
+    fig = plt.figure(219033)
     if len(ns) == 1: 
+        if projections == None: projections = [20]
+        fs = fs.project(projections)
         dadi.Plotting.plot_1d_comp_multinom(model, fs)   
     if len(ns) == 2:
+        if projections == None: projections = [20, 20]
+        fs = fs.project(projections)
         dadi.Plotting.plot_2d_comp_multinom(model, fs, vmin=vmin, resid_range=resid_range)
+    fig.savefig(output) 
 
 
-def plot_fitted_dfe(fs, cache, selection_params, projections, misid, output, vmin, resid_range):
+def plot_fitted_dfe(fs, cache1d, cache2d, demo_popt, sele_popt, ns_s, projections, pdf, pdf2, misid, output, vmin, resid_range):
 
     import dadi.DFE
     from Distribs import get_dadi_pdf
+    
+    fs = dadi.Spectrum.from_file(fs)
+    theta = ns_s * float(open(demo_popt, 'r').readline().rstrip().split()[-1])
+    sele_popt = np.array(open(sele_popt, 'r').readline().rstrip().split()[1:], dtype=float)
+    pdf = get_dadi_pdf(pdf)
+    pdf2 = get_dadi_pdf(pdf2)
+
+    ns = fs.sample_sizes
+    # Integrate over a range of gammas
+    pts_l = [ns[0]+10, ns[0]+20, ns[0]+30]
+    if cache1d != None
+        spectra1d = pickle.load(open(cache1d, 'rb'))
+        func = spectra1d.integrate
+    if cache2d != None
+        spectra2d = pickle.load(open(cache2d, 'rb'))
+        func = spectra2d.integrate
+    if (cache1d != None) and (cache2d != None): 
+        func = dadi.DFE.mixture
+    
+    if misid: func = dadi.Numerics.make_anc_state_misid_func(func) 
+    # Get expected SFS for MLE
+    if (cache1d != None) and (cache2d != None):
+        model = func(sele_popt, None, spectra1d, spectra2d, pdf, pdf2, theta, None)
+    else:
+        model = func(sele_popt, None, pdf, theta, None)
+
+    fig = plt.figure(219033)
+    if len(ns) == 1:
+        if projections == None: projections = [20]
+        fs = fs.project(projections)
+        model = model.project(projections)
+        dadi.Plotting.plot_1d_comp_multinom(model, fs)
+    if len(ns) == 2:
+        if projections == None: projections = [20, 20]
+        fs = fs.project(projections)
+        model = model.project(projections)
+        dadi.Plotting.plot_2d_comp_multinom(model, fs, vmin=vmin, resid_range=resid_range)
+    fig.savefig(output)
--- src/Stat.py
+++ src/Stat.py
@@ -7,13 +7,13 @@ from Models import get_dadi_model_func
 from Distribs import get_dadi_pdf
 
 def godambe_stat(fs, model, cache1d, cache2d, sele_dist, sele_dist2, ns_s, output,
-                 bootstrap_dir, pi, popt, popt_simple, misid, lrt, logscale):
+                 bootstrap_dir, pi, demo_popt, popt, popt_simple, misid, lrt, logscale):
 
     popt = np.array(open(popt, 'r').readline().rstrip().split(), dtype=float)
-    theta = ns_s * popt[-1]
+    theta = ns_s * float(open(demo_popt, 'r').readline().rstrip().split()[-1])
+
     if lrt:
         popt_simple = np.array(open(popt_simple, 'r').readline().rstrip().split(), dtype=float)
-        theta = ns_s * popt_simple[-1]
 
     fs = dadi.Spectrum.from_file(fs)
     fs_files = glob.glob(bootstrap_dir + '/*.fs')
@@ -38,10 +38,10 @@ def godambe_stat(fs, model, cache1d, cache2d, sele_dist, sele_dist2, ns_s, outpu
         func = dadi.DFE.mixture
         if misid:
             func = dadi.Numerics.make_anc_state_misid_func(func)
-        def model_func(pin, ns, pts):
-            #for mixture model, pin = [mu, sigma, p0, misid]
+        def model_func(params, ns, pts):
+            # for mixture model, pin = [mu, sigma, p0, misid]
             # Add in gammapos parameter
-            params = np.concatenate([pin[0:2], [0], pin[2:]])
+            # params = np.concatenate([pin[0:2], [0], pin[2:]])
             return func(params, None, s1, s2, sele_dist, 
                         sele_dist2, theta, None, exterior_int=True)
 
@@ -49,7 +49,7 @@ def godambe_stat(fs, model, cache1d, cache2d, sele_dist, sele_dist2, ns_s, outpu
     if lrt:
         ll_complex = popt[0]
         ll_simple = popt_simple[0]
-        adj = dadi.Godambe.LRT_adjust(model_func, [], all_boot, popt_simple[1:-1], fs,
+        adj = dadi.Godambe.LRT_adjust(model_func, [], all_boot, popt_simple[1:], fs,
                                       nested_indices=[pi-1], multinom=False, boot_theta_adjusts=boot_theta_adjusts)
         D_adj = adj*2*(ll_complex - ll_simple)
         pval = dadi.Godambe.sum_chi2_ppf(D_adj, weights=(0.5,0.5))
@@ -58,15 +58,15 @@ def godambe_stat(fs, model, cache1d, cache2d, sele_dist, sele_dist2, ns_s, outpu
             f.write('Adjusted D statistic: {0}'.format(D_adj) + '\n') 
             f.write('p-value for rejecting the simple model: {0}'.format(pval) + '\n')
     else:
-        uncerts_adj = dadi.Godambe.GIM_uncert(model_func, [], all_boot, popt[1:-1],
+        uncerts_adj = dadi.Godambe.GIM_uncert(model_func, [], all_boot, popt[1:],
                                               fs, multinom=False, eps=1e-4, log=logscale,
                                               boot_theta_adjusts=boot_theta_adjusts)
 
         with open(output, 'w') as f:
             f.write('Estimated 95% uncerts (theta adj): {0}'.format(1.96*uncerts_adj) + '\n')
             if logscale:
-                f.write('Lower bounds of 95% confidence interval : {0}'.format(np.exp(np.log(popt[1:-1])-1.96*uncerts_adj)) + '\n')
-                f.write('Upper bounds of 95% confidence interval : {0}'.format(np.exp(np.log(popt[1:-1])+1.96*uncerts_adj)) + '\n')
+                f.write('Lower bounds of 95% confidence interval : {0}'.format(np.exp(np.log(popt[1:])-1.96*uncerts_adj)) + '\n')
+                f.write('Upper bounds of 95% confidence interval : {0}'.format(np.exp(np.log(popt[1:])+1.96*uncerts_adj)) + '\n')
             else:
-                f.write('Lower bounds of 95% confidence interval : {0}'.format(popt[1:-1]-1.96*uncerts_adj) + '\n')
-                f.write('Upper bounds of 95% confidence interval : {0}'.format(popt[1:-1]+1.96*uncerts_adj) + '\n')
+                f.write('Lower bounds of 95% confidence interval : {0}'.format(popt[1:]-1.96*uncerts_adj) + '\n')
+                f.write('Upper bounds of 95% confidence interval : {0}'.format(popt[1:]+1.96*uncerts_adj) + '\n')
