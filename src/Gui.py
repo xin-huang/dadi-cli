25480ffe3c2d0b43a9ad543141cf8c6165985331
Xin Huang <xin-huang@users.noreply.github.com>
2021-02-21 11:08:23 -0700

    20210221 Update Gui

--- src/Gui.py
+++ src/Gui.py
@@ -5,6 +5,8 @@ from tkinter.filedialog import askopenfilename, asksaveasfilename, askdirectory
 from GenerateFs import generate_fs
 from Models import get_dadi_model_params
 
+import threading
+
 class analysis_window:
     # window template for different analysis
     def __init__(self, master, title):
@@ -80,19 +82,21 @@ class generate_fs_window(analysis_window):
             for e in self.params_entries:
                 sample_sizes.append(int(e.get()))
                 
-            generate_fs(
-                vcf=self.lbl_vcf['text'], 
-                output=self.lbl_output['text'], 
-                pop_ids=self.pop_ids, 
-                pop_info=self.lbl_pop_info['text'], 
-                projections=sample_sizes, 
-                polarized=is_polarized, 
-                bootstrap=None, chunk_size=0)
+            #generate_fs(
+            #    vcf=self.lbl_vcf['text'], 
+            #    output=self.lbl_output['text'], 
+            #    pop_ids=self.pop_ids, 
+            #    pop_info=self.lbl_pop_info['text'], 
+            #    projections=sample_sizes, 
+            #    polarized=is_polarized, 
+            #    bootstrap=None, chunk_size=0)
+            t = threading.Thread(target=generate_fs, args=(self.lbl_vcf['text'], self.lbl_output['text'], self.pop_ids, self.lbl_pop_info['text'], sample_sizes, is_polarized, None, 0))
+            t.start()
         
         self.btn_select_vcf = Button(self.frm, text="Select a VCF file", command=select_vcf_file)
         self.btn_select_pop_info = Button(self.frm, text="Select a Pop Info file", command=select_pop_info)
         self.btn_select_output = Button(self.frm, text="Save as", command=output_file)
-        self.btn_generate = Button(self.frm, width=10, text="Generate", command=run_generate_fs)
+        self.btn_generate = Button(self.frm, width=10, text="Submit", command=run_generate_fs)
         
         self.lbl_vcf = Label(self.frm, text="No VCF file selected")
         self.lbl_pop_info = Label(self.frm, text="No Pop Info file selected")
@@ -121,57 +125,53 @@ class infer_dm_window(analysis_window):
     def __init__(self, master):
         analysis_window.__init__(self, master, "Infer demographic models")
         
-        frm = Frame(self.window)
-        frm_initial_values = Frame(frm)
-        frm_upper_bounds = Frame(frm)
-        frm_lower_bounds = Frame(frm)
-        frm_fixed_params = Frame(frm)
-        params_labels = []
-        params_entries = []
-        params_checkboxes = []
+        self.frm = Frame(self.window)
+        self.frm_initial_values = Frame(self.frm)
+        self.frm_upper_bounds = Frame(self.frm)
+        self.frm_lower_bounds = Frame(self.frm)
+        self.frm_fixed_params = Frame(self.frm)
+        self.params_labels = []
+        self.params_entries = []
+        self.params_checkboxes = []
         
         def select_fs_file():
             file_name = askopenfilename(
-                parent=window,
+                parent=self.window,
                 filetypes=[("Frequency Spectrum Files", "*.fs"), ("All Files", "*.*")]
             )
             if not file_name: return
             
-            lbl_fs['text'] = file_name
-            
-            return file_name
+            self.lbl_fs['text'] = file_name
         
         def select_output_dir():
-            file_dir = askdirectory(parent=window)
+            file_dir = askdirectory(parent=self.window)
             
             if not file_dir: return
             
-            lbl_output_dir['text'] = file_dir
-            
-            return file_dir
+            self.lbl_output_dir['text'] = file_dir
         
         def run_infer_dm():
             return
         
-        btn_select_fs = Button(frm, text="Select a frequency spectrum file", command=select_fs_file)
-        lbl_fs = Label(frm, text="No frequency spectrum file selected")   
-        btn_select_fs.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-        lbl_fs.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
+        self.btn_select_fs = Button(self.frm, text="Select a frequency spectrum file", command=select_fs_file)
+        self.lbl_fs = Label(self.frm, text="No frequency spectrum file selected")   
+        self.btn_select_fs.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        self.lbl_fs.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
         
-        btn_select_output_dir = Button(frm, text="Select a output directory", command=select_output_dir)
-        lbl_output_dir = Label(frm, text="No output directory selected")
-        btn_select_output_dir.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
-        lbl_output_dir.grid(row=1, column=1, sticky="ew", padx=5, pady=5)
+        self.btn_select_output_dir = Button(self.frm, text="Select a output directory", command=select_output_dir)
+        self.lbl_output_dir = Label(self.frm, text="No output directory selected")
+        self.btn_select_output_dir.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+        self.lbl_output_dir.grid(row=1, column=1, sticky="ew", padx=5, pady=5)
         
-        lbl_output_file_prefix = Label(frm, text="Output file prefix")
-        ent_output_file = Entry(frm, width=50)
-        lbl_output_file_prefix.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
-        ent_output_file.grid(row=2, column=1, sticky="ew", padx=5, pady=5)
+        self.lbl_output_file_prefix = Label(self.frm, text="Output file prefix")
+        self.ent_output_file = Entry(self.frm, width=50)
+        self.lbl_output_file_prefix.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+        self.ent_output_file.grid(row=2, column=1, sticky="ew", padx=5, pady=5)
         
-        lbl_model = Label(frm, text="Select a demographic model")
-        model_value = StringVar()
-        cbb_models = ttk.Combobox(frm, textvariable=model_value)
-        cbb_models['values'] = (
+        self.lbl_model = Label(self.frm, text="Select a demographic model")
+        self.model_value = StringVar()
+        self.cbb_models = ttk.Combobox(self.frm, textvariable=self.model_value)
+        self.cbb_models['values'] = (
             'bottlegrowth_1d',
             'growth_1d',
             'snm_1d',
@@ -186,33 +186,33 @@ class infer_dm_window(analysis_window):
             'split_asym_mig',
             'snm_2d'
         )
-        cbb_models.state(["readonly"])
+        self.cbb_models.state(["readonly"])
         
         def select_models(event):
             
-            for lbl in params_labels: lbl.destroy()
-            for ent in params_entries: ent.destroy()
-            for cbt in params_checkboxes: cbt.destroy()
+            for lbl in self.params_labels: lbl.destroy()
+            for ent in self.params_entries: ent.destroy()
+            for cbt in self.params_checkboxes: cbt.destroy()
             
-            params = get_dadi_model_params(cbb_models.get())
+            params = get_dadi_model_params(self.cbb_models.get())
             i = 0
             for p in params:
-                lbl1 = Label(frm_initial_values, text=p)
-                lbl2 = Label(frm_upper_bounds, text=p)
-                lbl3 = Label(frm_lower_bounds, text=p)
-                params_labels.append(lbl1)
-                params_labels.append(lbl2)
-                params_labels.append(lbl3)
+                lbl1 = Label(self.frm_initial_values, text=p)
+                lbl2 = Label(self.frm_upper_bounds, text=p)
+                lbl3 = Label(self.frm_lower_bounds, text=p)
+                self.params_labels.append(lbl1)
+                self.params_labels.append(lbl2)
+                self.params_labels.append(lbl3)
                 
-                ent1 = Entry(frm_initial_values, width=10)
-                ent2 = Entry(frm_upper_bounds, width=10)
-                ent3 = Entry(frm_lower_bounds, width=10)
-                params_entries.append(ent1)
-                params_entries.append(ent2)
-                params_entries.append(ent3)
+                ent1 = Entry(self.frm_initial_values, width=10)
+                ent2 = Entry(self.frm_upper_bounds, width=10)
+                ent3 = Entry(self.frm_lower_bounds, width=10)
+                self.params_entries.append(ent1)
+                self.params_entries.append(ent2)
+                self.params_entries.append(ent3)
                 
-                cbt = Checkbutton(frm_fixed_params, text=p)
-                params_checkboxes.append(cbt)
+                cbt = Checkbutton(self.frm_fixed_params, text=p)
+                self.params_checkboxes.append(cbt)
                 
                 lbl1.grid(row=0, column=i, sticky="ew", padx=5, pady=5)
                 lbl2.grid(row=0, column=i, sticky="ew", padx=5, pady=5)
@@ -224,36 +224,36 @@ class infer_dm_window(analysis_window):
                 ent3.grid(row=0, column=i, sticky="ew", padx=5, pady=5)
                 i += 1
             
-        cbb_models.bind('<<ComboboxSelected>>', select_models)
+        self.cbb_models.bind('<<ComboboxSelected>>', select_models)
         
-        lbl_model.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
-        cbb_models.grid(row=3, column=1, sticky="ew", padx=5, pady=5)
+        self.lbl_model.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
+        self.cbb_models.grid(row=3, column=1, sticky="ew", padx=5, pady=5)
         
-        cbt_misid = Checkbutton(frm, text="Add misidentification")
-        cbt_misid.grid(row=4, column=0, sticky="n", padx=5, pady=5)
+        self.cbt_misid = Checkbutton(self.frm, text="Add misidentification")
+        self.cbt_misid.grid(row=4, column=0, sticky="n", padx=5, pady=5)
         
-        lbl_initial_values = Label(frm, text="Initial values")
-        lbl_upper_bounds = Label(frm, text="Upper bounds")
-        lbl_lower_bounds = Label(frm, text="Lower bounds")
-        lbl_fixed_params = Label(frm, text="Fixed parameters")
-        lbl_parallel_jobs = Label(frm, text="Parallel jobs")
-        lbl_initial_values.grid(row=5, column=0, sticky="ew", padx=5, pady=5)
-        lbl_upper_bounds.grid(row=6, column=0, sticky="ew", padx=5, pady=5)
-        lbl_lower_bounds.grid(row=7, column=0, sticky="ew", padx=5, pady=5)
-        lbl_fixed_params.grid(row=8, column=0, sticky="ew", padx=5, pady=5)
-        lbl_parallel_jobs.grid(row=9, column=0, sticky="ew", padx=5, pady=5)
+        self.lbl_initial_values = Label(self.frm, text="Initial values")
+        self.lbl_upper_bounds = Label(self.frm, text="Upper bounds")
+        self.lbl_lower_bounds = Label(self.frm, text="Lower bounds")
+        self.lbl_fixed_params = Label(self.frm, text="Fixed parameters")
+        self.lbl_parallel_jobs = Label(self.frm, text="Parallel jobs")
+        self.lbl_initial_values.grid(row=5, column=0, sticky="ew", padx=5, pady=5)
+        self.lbl_upper_bounds.grid(row=6, column=0, sticky="ew", padx=5, pady=5)
+        self.lbl_lower_bounds.grid(row=7, column=0, sticky="ew", padx=5, pady=5)
+        self.lbl_fixed_params.grid(row=8, column=0, sticky="ew", padx=5, pady=5)
+        self.lbl_parallel_jobs.grid(row=9, column=0, sticky="ew", padx=5, pady=5)
         
-        ent_parallel_jobs = Entry(frm, width=20)
-        ent_parallel_jobs.grid(row=9, column=1, sticky="ew", padx=5, pady=5)
+        self.ent_parallel_jobs = Entry(self.frm, width=20)
+        self.ent_parallel_jobs.grid(row=9, column=1, sticky="ew", padx=5, pady=5)
         
-        btn_infer = Button(frm, width=10, text="Infer", command=run_infer_dm)
-        btn_infer.grid(row=10, column=2, sticky="ew", padx=5, pady=5)
+        self.btn_infer = Button(self.frm, width=10, text="Submit", command=run_infer_dm)
+        self.btn_infer.grid(row=10, column=2, sticky="ew", padx=5, pady=5)
         
-        frm_initial_values.grid(row=5, column=1, sticky="ew", padx=5, pady=5)
-        frm_upper_bounds.grid(row=6, column=1, sticky="ew", padx=5, pady=5)
-        frm_lower_bounds.grid(row=7, column=1, sticky="ew", padx=5, pady=5)
-        frm_fixed_params.grid(row=8, column=1, sticky="ew", padx=5, pady=5)
-        frm.grid(row=0, column=0, sticky="ns")
+        self.frm_initial_values.grid(row=5, column=1, sticky="ew", padx=5, pady=5)
+        self.frm_upper_bounds.grid(row=6, column=1, sticky="ew", padx=5, pady=5)
+        self.frm_lower_bounds.grid(row=7, column=1, sticky="ew", padx=5, pady=5)
+        self.frm_fixed_params.grid(row=8, column=1, sticky="ew", padx=5, pady=5)
+        self.frm.grid(row=0, column=0, sticky="ns")
         
 def main():
     # create the root window
