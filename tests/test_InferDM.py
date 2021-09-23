0ac4962c3779a7fb007841070a800eec4893498e
tjstruck <tjstruck@email.arizona.edu>
2021-09-22 18:50:18 -0700

    Fix missing comma in subprocess.run test_InferDM.py

--- tests/test_InferDM.py
+++ tests/test_InferDM.py
@@ -34,7 +34,7 @@ def test_InferDM_wq(capsys):
     "--fs", "./example_data/two_epoch_syn.fs", "--model", "two_epoch_1d", "--grids", 
     "120", "140", "160", "--p0", "1", ".5", "--ubounds", "10", "10", "--lbounds", "10e-3", "10e-3",
     "--output", "./test_results/simulation.two_epoch.demo.params.wq", "--thread", str(threads), "--check-convergence", 
-    "--work-queue", "test-two-epoch", "mypwfile", "&" 
+    "--work-queue", "test-two-epoch", "mypwfile", "&",
     "work_queue_factory", "-T", "local", "-M", "test-two-epoch", "-P", "mypwfile", "--workers-per-cycle", "0", "--cores=1"
     ])
     fits = glob.glob("./test_results/simulation.two_epoch.demo.params.wq.InferDM.opts.*")
