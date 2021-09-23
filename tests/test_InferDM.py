d94912dcce6d3e516014a0a0f514d282ef009b73
tjstruck <tjstruck@email.arizona.edu>
2021-09-22 19:19:59 -0700

    Change --output to --output-prefix

--- tests/test_InferDM.py
+++ tests/test_InferDM.py
@@ -15,9 +15,9 @@ def test_InferDM(capsys):
     threads = 3
     subprocess.run(
         "dadi-cli InferDM " + 
-        "--fs ./example_data/two_epoch_syn.fs --model two_epoch_1d --grids 120 140 160 " +
-        "--p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 " + 
-        "--output ./test_results/simulation.two_epoch.demo.params --thread " + str(threads), shell=True
+        "--fs ./example_data/two_epoch_syn.fs --model two_epoch_1d " +
+        "--grids 120 140 160 --p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 " + 
+        "--output-prefix ./test_results/simulation.two_epoch.demo.params --thread " + str(threads), shell=True
     )
     fits = glob.glob("./test_results/simulation.two_epoch.demo.params.InferDM.opts.*")
     number_of_fits = sum([ele.startswith('#') != True for ele in open(fits[-1]).readlines()])
@@ -31,9 +31,9 @@ def test_InferDM_wq(capsys):
     threads = 3
     subprocess.run(
         "dadi-cli InferDM " +
-        "--fs ./example_data/two_epoch_syn.fs --model two_epoch_1d --grids " +
-        "120 140 160 --p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 " +
-        "--output ./test_results/simulation.two_epoch.demo.params.wq --thread " + str(threads) + " --check-convergence " +
+        "--fs ./example_data/two_epoch_syn.fs --model two_epoch_1d " +
+        "--grids 120 140 160 --p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 " +
+        "--output-prefix ./test_results/simulation.two_epoch.demo.params.wq --thread " + str(threads) +
         "--work-queue test-dm-two-epoch mypwfile " +
         "& work_queue_factory -T local -M test-dm-two-epoch -P mypwfile --workers-per-cycle 0 --cores=1", shell=True
     )
