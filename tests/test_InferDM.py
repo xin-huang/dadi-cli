30a01bb12682b71a7b9b3156a7821d4b0fc6faaa
tjstruck <tjstruck@email.arizona.edu>
2021-09-22 19:04:04 -0700

    Update suprocess usage in test_InferDM.py

--- tests/test_InferDM.py
+++ tests/test_InferDM.py
@@ -13,30 +13,30 @@ except:
 
 def test_InferDM(capsys):
     threads = 3
-    subprocess.run([
-        "dadi-cli", "InferDM",
-        "--fs", "./example_data/two_epoch_syn.fs", "--model", "two_epoch_1d", "--grids", "120", "140", "160", 
-        "--p0", "1", ".5", "--ubounds", "10", "10", "--lbounds", "10e-3", "10e-3",
-        "--output", "./test_results/simulation.two_epoch.demo.params", "--thread", str(threads)
-    ])
+    subprocess.run(
+        "dadi-cli InferDM " + 
+        "--fs ./example_data/two_epoch_syn.fs --model two_epoch_1d --grids 120 140 160 " +
+        "--p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 " + 
+        "--output ./test_results/simulation.two_epoch.demo.params --thread " + str(threads), shell=True
+    )
     fits = glob.glob("./test_results/simulation.two_epoch.demo.params.InferDM.opts.*")
     number_of_fits = sum([ele.startswith('#') != True for ele in open(fits[-1]).readlines()])
     assert threads == number_of_fits
 
 
 # dadi-cli InferDM --fs ./example_data/two_epoch_syn.fs --model two_epoch_1d --grids 120 140 160 --p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 \
-# --output ./test_results/simulation.two_epoch.demo.params.wq --thread 3 --check-convergence --work-queue test-two-epoch mypwfile &
+# --output ./test_results/simulation.two_epoch.demo.params.wq --thread 3 --work-queue test-two-epoch mypwfile &
 # work_queue_factory -T local -M test-two-epoch -P mypwfile --workers-per-cycle 0 --cores=1
 def test_InferDM_wq(capsys):
     threads = 3
-    subprocess.run([
-    "dadi-cli", "InferDM", 
-    "--fs", "./example_data/two_epoch_syn.fs", "--model", "two_epoch_1d", "--grids", 
-    "120", "140", "160", "--p0", "1", ".5", "--ubounds", "10", "10", "--lbounds", "10e-3", "10e-3",
-    "--output", "./test_results/simulation.two_epoch.demo.params.wq", "--thread", str(threads), "--check-convergence", 
-    "--work-queue", "test-two-epoch", "mypwfile", "&",
-    "work_queue_factory", "-T", "local", "-M", "test-two-epoch", "-P", "mypwfile", "--workers-per-cycle", "0", "--cores=1"
-    ])
+    subprocess.run(
+        "dadi-cli InferDM " +
+        "--fs ./example_data/two_epoch_syn.fs --model two_epoch_1d --grids " +
+        "120 140 160 --p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 " +
+        "--output ./test_results/simulation.two_epoch.demo.params.wq --thread " + str(threads) + " --check-convergence " +
+        "--work-queue test-two-epoch mypwfile " +
+        "& work_queue_factory -T local -M test-two-epoch -P mypwfile --workers-per-cycle 0 --cores=1", shell=True
+    )
     fits = glob.glob("./test_results/simulation.two_epoch.demo.params.wq.InferDM.opts.*")
     number_of_fits = sum([ele.startswith('#') != True for ele in open(fits[-1]).readlines()])
     assert threads == number_of_fits
