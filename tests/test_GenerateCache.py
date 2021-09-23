82c9a05b24d189d8723b2e904a532f70df1a931f
tjstruck <tjstruck@email.arizona.edu>
2021-09-23 11:07:06 -0700

    Start simple test for GenerateCache

---
+++ tests/test_GenerateCache.py
@@ -0,0 +1,22 @@
+import dadi
+import pytest
+import subprocess
+from os.path import exists
+import numpy as np
+import pickle
+
+def test_BestFit(capsys):
+    min_gamma_bound = -2000
+    gamma_pts = 50
+    subprocess.run(
+        "dadi-cli GenerateCache --demo-popt ./test_results/simulation.two_epoch.demo.params.InferDM.bestfits " +
+        "--gamma-bounds 1e-4 " + str(min_gamma_bound) + " --gamma-pts " + str(gamma_pts) + 
+        " --grids 120 140 160 --model two_epoch " + 
+        "--output ./test_results/cache_large_two_epoch.bpkl --sample-sizes 20", shell=True
+    )
+    assert exists("./test_results/cache_large_two_epoch.bpkl")
+    s = pickle.load(open('./test_results/cache_large_two_epoch.bpkl','rb'))
+    assert int(np.min(s.gammas)) == min_gamma_bound
+    assert len(s.gammas) == gamma_pts
+
+
