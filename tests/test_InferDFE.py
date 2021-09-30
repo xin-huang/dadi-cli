307e8db3cc740cecb1ccd9e1fdeda72fee3ea041
tjstruck <tjstruck@email.arizona.edu>
2021-09-30 12:41:09 -0700

    Add --factory-timeout=180 to work queue commands

--- tests/test_InferDFE.py
+++ tests/test_InferDFE.py
@@ -30,10 +30,6 @@ def test_InferDFE(capsys):
     number_of_fits = sum([ele.startswith('#') != True for ele in open(fits[-1]).readlines()])
     assert threads == number_of_fits
 
-
-# dadi-cli InferDM --fs ./example_data/two_epoch_syn.fs --model two_epoch_1d --grids 120 140 160 --p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 \
-# --output ./test_results/simulation.two_epoch.demo.params.wq --thread 3 --work-queue test-two-epoch mypwfile &
-# work_queue_factory -T local -M test-two-epoch -P mypwfile --workers-per-cycle 0 --cores=1
 def test_InferDM_wq(capsys):
     threads = 3
     subprocess.run(
@@ -43,7 +39,7 @@ def test_InferDM_wq(capsys):
         "--grids 120 140 160 --p0 1 1 --ubounds 10 10 --lbounds 10e-3 10e-3 " + 
         "--output-prefix ./test_results/simulation.two_epoch.dfe.params --thread " + str(threads) +
         "--work-queue test-dfe-two-epoch mypwfile " +
-        "& work_queue_factory -T local -M test-dfe-two-epoch -P mypwfile --workers-per-cycle 0 --cores=1", shell=True
+        "& work_queue_factory -T local -M test-dfe-two-epoch -P mypwfile --factory-timeout=180 --workers-per-cycle 0 --cores=1  -w " + str(threads), shell=True
     )
     fits = glob.glob("./test_results/simulation.two_epoch.dfe.params.wq.InferDM.opts.*")
     number_of_fits = sum([ele.startswith('#') != True for ele in open(fits[-1]).readlines()])
--- tests/test_InferDM.py
+++ tests/test_InferDM.py
@@ -1,6 +1,5 @@
 import dadi
 import pytest
-import numpy as np
 import subprocess
 import glob
 import os
@@ -26,7 +25,7 @@ def test_InferDM(capsys):
 
 # dadi-cli InferDM --fs ./example_data/two_epoch_syn.fs --model two_epoch_1d --grids 120 140 160 --p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 \
 # --output ./test_results/simulation.two_epoch.demo.params.wq --thread 3 --work-queue test-two-epoch mypwfile &
-# work_queue_factory -T local -M test-two-epoch -P mypwfile --workers-per-cycle 0 --cores=1
+# work_queue_worker -M test-two-epoch -P mypwfile --cores=1 --workers-per-cycle=0 -t 180 -w 3 --factory-timeout=600
 def test_InferDM_wq(capsys):
     threads = 3
     subprocess.run(
@@ -35,7 +34,7 @@ def test_InferDM_wq(capsys):
         "--grids 120 140 160 --p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 " +
         "--output-prefix ./test_results/simulation.two_epoch.demo.params.wq --thread " + str(threads) +
         "--work-queue test-dm-two-epoch mypwfile " +
-        "& work_queue_factory -T local -M test-dm-two-epoch -P mypwfile --workers-per-cycle 0 --cores=1", shell=True
+        "& work_queue_factory -T local -M test-dm-two-epoch -P mypwfile --factory-timeout=180 --workers-per-cycle=0 --cores=1  -w " + str(threads), shell=True
     )
     fits = glob.glob("./test_results/simulation.two_epoch.demo.params.wq.InferDM.opts.*")
     number_of_fits = sum([ele.startswith('#') != True for ele in open(fits[-1]).readlines()])
