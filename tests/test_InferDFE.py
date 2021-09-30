3bf07f1424539fb6a6272d29d0b548aace097174
tjstruck <tjstruck@email.arizona.edu>
2021-09-30 16:13:39 -0700

    Fix bugs with dadi-cli execution, and change directories/input date to make test_InferDFE.py independent

--- tests/test_InferDFE.py
+++ tests/test_InferDFE.py
@@ -13,12 +13,7 @@ except:
 
 def test_InferDFE(capsys):
     threads = 3
-    try:
-        fits_fid = glob.glob("./test_results/simulation.two_epoch.demo.params.InferDM.bestfits")[0]
-    except:
-        fid = open("./test_results/simulation.two_epoch.demo.params.InferDM.bestfits", "w")
-        fid.write("#title\n0\t0\t0\t1000\n")
-        fits_fid = "./test_results/simulation.two_epoch.demo.params.InferDM.bestfits_fake"
+    fits_fid = "./example_data/example.two_epoch.demo.params.InferDM.bestfits"
     subprocess.run(
         "dadi-cli InferDFE " + 
         "--fs ./example_data/two_epoch_non.fs --cache1d example_data/cache_two_epoch_1d.bpkl " +
@@ -32,14 +27,15 @@ def test_InferDFE(capsys):
 
 def test_InferDFE_wq(capsys):
     threads = 3
+    fits_fid = "./example_data/example.two_epoch.demo.params.InferDM.bestfits"
     subprocess.run(
         "dadi-cli InferDFE " + 
-        "--fs ./example_data/two_epoch_non.fs --cache1d cache_two_epoch_1d.bpkl " +
-        "--demo-popt two_epoch_1d --pdf1d lognormal --ratio 2.31 " +
-        "--grids 120 140 160 --p0 1 1 --ubounds 10 10 --lbounds 10e-3 10e-3 " + 
+        "--fs ./example_data/two_epoch_non.fs --cache1d example_data/cache_two_epoch_1d.bpkl " +
+        "--demo-popt " + fits_fid + " --pdf1d lognormal --ratio 2.31 " +
+        "--p0 1 1 --ubounds 10 10 --lbounds 10e-3 10e-3 " + 
         "--output-prefix ./test_results/simulation.two_epoch.dfe.params --thread " + str(threads) + ' ' +
         "--work-queue test-dfe-two-epoch mypwfile " +
-        "& work_queue_factory -T local -M test-dfe-two-epoch -P mypwfile --factory-timeout=180 --workers-per-cycle 0 --cores=1  -w " + str(threads), shell=True
+        "& work_queue_factory -T local -M test-dfe-two-epoch -P mypwfile --factory-timeout=180 --workers-per-cycle=0 --cores=1  -w " + str(threads), shell=True
     )
     fits = glob.glob("./test_results/simulation.two_epoch.dfe.params.wq.InferDM.opts.*")
     number_of_fits = sum([ele.startswith('#') != True for ele in open(fits[-1]).readlines()])
