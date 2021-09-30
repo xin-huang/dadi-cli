43b2f4d591b93f1816797780f9e1c0a0becc8a52
tjstruck <tjstruck@email.arizona.edu>
2021-09-30 15:45:14 -0700

    Make test_BestFit.py independent and add example optimization output to example_data

--- tests/test_BestFit.py
+++ tests/test_BestFit.py
@@ -5,7 +5,7 @@ from os.path import exists
 
 def test_BestFit(capsys):
     subprocess.run(
-        "dadi-cli BestFit --input-prefix ./test_results/simulation.two_epoch.demo.params.InferDM " +
+        "dadi-cli BestFit --input-prefix ./example_data/example.two_epoch.demo.params.InferDM " +
         "--model two_epoch_1d --lbounds 10e-3 10e-3 --ubounds 10 10", shell=True
     )
-    assert exists("./test_results/simulation.two_epoch.demo.params.InferDM.bestfits")
+    assert exists("./example_data/example.two_epoch.demo.params.InferDM.bestfits")
