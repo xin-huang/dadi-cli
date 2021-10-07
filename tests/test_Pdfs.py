661a3fac81756a95486b22af8c37b73a425c8c45
xin-huang <xin.huang07@gmail.com>
2021-10-07 13:31:02 -0700

    20211007 Update test_Pdfs.py

--- tests/test_Pdfs.py
+++ tests/test_Pdfs.py
@@ -1,12 +1,24 @@
 import dadi
 import pytest
-from scr.Pdfs import get_dadi_pdf, get_dadi_pdf_params, print_available_pdfs, print_pdf_details
+import numpy as np
+from src.Pdfs import get_dadi_pdf, get_dadi_pdf_params, print_available_pdfs, print_pdf_details
 
 def test_get_dadi_pdf():
     pass
 
 def test_get_dadi_pdf_params():
-    pass
+    assert np.array_equal(get_dadi_pdf_params('beta'), ['alpha', 'beta'])
+    assert np.array_equal(get_dadi_pdf_params('biv_sym_ind_gamma'), ['shape', 'scale'])
+    assert np.array_equal(get_dadi_pdf_params('biv_asym_ind_gamma'), ['shape1', 'scale1', 'shape2', 'scale2'])
+    assert np.array_equal(get_dadi_pdf_params('biv_sym_lognormal'), ['log(mu)', 'log(sigma)', 'rho'])
+    assert np.array_equal(get_dadi_pdf_params('biv_asym_lognormal'), ['log(mu1)', 'log(sigma1)', 'log(mu2)', 'log(sigma2)', 'rho'])
+    assert np.array_equal(get_dadi_pdf_params('exponential'), ['scale'])
+    assert np.array_equal(get_dadi_pdf_params('gamma'), ['shape', 'scale'])
+    assert np.array_equal(get_dadi_pdf_params('lognormal'), ['log(mu)', 'log(sigma)'])
+    assert np.array_equal(get_dadi_pdf_params('normal'), ['mu', 'sigma'])
+
+    with pytest.raises(Exception) as e_info:
+        get_dadi_pdf_params('haha')
 
 def test_print_available_pdfs():
     pass
