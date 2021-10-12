3a2d286c666e58fca24b2caaaa49b618c2234203
tjstruck <tjstruck@email.arizona.edu>
2021-10-12 10:16:20 -0700

    20211012 Update InferDM and InferDFE tests for new optimizations option

--- tests/test_InferDFE.py
+++ tests/test_InferDFE.py
@@ -12,14 +12,14 @@ except:
     pass
 
 def test_InferDFE(capsys):
-    threads = 3
+    threads = 5
     fits_fid = "./tests/example_data/example.two_epoch.demo.params.InferDM.bestfits"
     subprocess.run(
         "dadi-cli InferDFE " + 
         "--fs ./tests/example_data/two_epoch_non.fs --cache1d ./tests/example_data/cache_two_epoch_1d.bpkl " +
         "--demo-popt " + fits_fid + " --pdf1d lognormal --ratio 2.31 " +
         "--p0 1 1 --ubounds 10 10 --lbounds 10e-3 10e-3 " + 
-        "--output-prefix ./tests/test_results/simulation.two_epoch.dfe.params --thread " + str(threads), shell=True
+        "--output-prefix ./tests/test_results/simulation.two_epoch.dfe.params --optimizations " + str(threads), shell=True
     )
     fits = glob.glob("./tests/test_results/simulation.two_epoch.dfe.params.InferDFE.opts.*")
     number_of_fits = sum([ele.startswith('#') != True for ele in open(fits[-1]).readlines()])
@@ -27,14 +27,14 @@ def test_InferDFE(capsys):
 
 @pytest.mark.skip(reason="no way of currently testing this")
 def test_InferDFE_wq(capsys):
-    threads = 3
+    threads = 5
     fits_fid = "./tests/example_data/example.two_epoch.demo.params.InferDM.bestfits"
     subprocess.run(
         "dadi-cli InferDFE " + 
         "--fs ./tests/example_data/two_epoch_non.fs --cache1d ./tests/example_data/cache_two_epoch_1d.bpkl " +
         "--demo-popt " + fits_fid + " --pdf1d lognormal --ratio 2.31 " +
         "--p0 1 1 --ubounds 10 10 --lbounds 10e-3 10e-3 " + 
-        "--output-prefix ./tests/test_results/simulation.two_epoch.dfe.params --thread " + str(threads) + ' ' +
+        "--output-prefix ./tests/test_results/simulation.two_epoch.dfe.params --optimizations " + str(threads) + ' ' +
         "--work-queue test-dfe-two-epoch ./tests/mypwfile " +
         "& work_queue_factory -T local -M test-dfe-two-epoch -P mypwfile --factory-timeout=180 --workers-per-cycle=0 --cores=1  -w " + str(threads), shell=True
     )
--- tests/test_InferDM.py
+++ tests/test_InferDM.py
@@ -11,12 +11,12 @@ except:
     pass
 
 def test_InferDM(capsys):
-    threads = 3
+    threads = 5
     subprocess.run(
         "dadi-cli InferDM " + 
         "--fs ./tests/example_data/two_epoch_syn.fs --model two_epoch " +
         "--grids 120 140 160 --p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 " + 
-        "--output-prefix ./tests/test_results/simulation.two_epoch.demo.params --thread " + str(threads), shell=True
+        "--output-prefix ./tests/test_results/simulation.two_epoch.demo.params --optimizations " + str(threads), shell=True
     )
     fits = glob.glob("./tests/test_results/simulation.two_epoch.demo.params.InferDM.opts.*")
     number_of_fits = sum([ele.startswith('#') != True for ele in open(fits[-1]).readlines()])
@@ -28,12 +28,12 @@ def test_InferDM(capsys):
 # work_queue_worker -M test-two-epoch -P mypwfile --cores=1 --workers-per-cycle=0 -t 180 -w 3 --factory-timeout=600
 @pytest.mark.skip(reason="no way of currently testing this")
 def test_InferDM_wq(capsys):
-    threads = 3
+    threads = 5
     subprocess.run(
         "dadi-cli InferDM " +
         "--fs ./tests/example_data/two_epoch_syn.fs --model two_epoch " +
         "--grids 120 140 160 --p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 " +
-        "--output-prefix ./tests/test_results/simulation.two_epoch.demo.params.wq --thread " + str(threads) + ' ' +
+        "--output-prefix ./tests/test_results/simulation.two_epoch.demo.params.wq --optimizations " + str(threads) + ' ' +
         "--work-queue test-dm-two-epoch ./tests/mypwfile " +
         "& work_queue_factory -T local -M test-dm-two-epoch -P mypwfile --factory-timeout=180 --workers-per-cycle=0 --cores=1  -w " + str(threads), shell=True
     )
