ca48374a631ae80de485ebd27911962376fa693d
tjstruck <tjstruck@email.arizona.edu>
2021-10-13 13:49:46 -0700

    20211013 Uncomment work queue process

--- tests/test_InferDM.py
+++ tests/test_InferDM.py
@@ -26,7 +26,7 @@ def test_InferDM(capsys):
 # dadi-cli InferDM --fs ./example_data/two_epoch_syn.fs --model two_epoch_1d --grids 120 140 160 --p0 1 .5 --ubounds 10 10 --lbounds 10e-3 10e-3 \
 # --output ./test_results/simulation.two_epoch.demo.params.wq --thread 3 --work-queue test-two-epoch mypwfile &
 # work_queue_worker -M test-two-epoch -P mypwfile --cores=1 --workers-per-cycle=0 -t 180 -w 3 --factory-timeout=600
-@pytest.mark.skip(reason="no way of currently testing this")
+# @pytest.mark.skip(reason="no way of currently testing this")
 def test_InferDM_wq(capsys):
     threads = 3
     fits_fid = "./tests/example_data/example.two_epoch.demo.params.InferDM.bestfits"
