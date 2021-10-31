9e492486a5e018d9cb2d836945acafb955adfb48
xin-huang <xin.huang07@gmail.com>
2021-10-31 22:52:50 +0100

    20211031 Add test_generate_fs_args

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,1 +1,15 @@
 import pytest
+import src.__main__ as cli
+
+def test_generate_fs_args():
+    parser = cli.dadi_cli_parser()
+    cmd = "GenerateFs"
+    vcf = "test.vcf"
+    pop_infos = "test.info"
+    output = "test.output"
+    args = parser.parse_args([cmd, "--vcf", vcf, "--pop-info", pop_infos, "--pop-ids", "YRI", "--projections", "216", "--output", output])
+    assert args.vcf == vcf
+    assert args.pop_info == pop_infos
+    assert args.pop_ids == ['YRI']
+    assert args.projections == [216]
+    assert args.output == output
