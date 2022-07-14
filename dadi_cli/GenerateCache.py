0d2453ca2f8063a12b81b4776939b34cb7917e92
tjstruck <tjstruck@email.arizona.edu>
2022-07-14 14:17:26 -0700

    Update get_opts_and_theta to change popt parsing excluding misid parameter to be only for generate cache

--- dadi_cli/GenerateCache.py
+++ dadi_cli/GenerateCache.py
@@ -28,7 +28,7 @@ def generate_cache(func, grids, popt,
         dimensionality int: Dimensionality of the frequency spectrum.
     """
 
-    popt, theta = get_opts_and_theta(popt)
+    popt, theta = get_opts_and_theta(popt, True)
 
     if cuda: dadi.cuda_enabled(True)
 
