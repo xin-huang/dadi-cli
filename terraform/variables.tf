764beda7a3131d868ef8c00cf9a09546a008bb55
Sean Davey <sdavey@arizona.edu>
2022-09-15 06:59:31 -0700

    switched UDP to TCP for Work Queue, made instance_type a variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
+
+variable "instance_type" {
+  description = "The type of AWS EC2 instance to deploy"
+  default = "t3.xlarge"
+}
+
 variable "region" {
   description = "The region Terraform deploys your instance"
   default = "us-east-2"
