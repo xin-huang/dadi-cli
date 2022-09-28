5436b8776e2e560824adfcbc46689c374916e1eb
tjstruck <tjstruck@email.arizona.edu>
2022-09-27 17:22:58 -0700

    Change wq test to not use custom model, add tests for misid and bestfit file import

--- tests/test_main.py
+++ tests/test_main.py
@@ -37,24 +37,55 @@ def infer_dm_args():
     pytest.debug_wq = False
     pytest.cpus = 1
     pytest.gpus = 0
-    pytest.optimizations = 3
+    pytest.optimizations = 1
     pytest.bestfit_p0 = None
     pytest.delta_ll = 0.001
     pytest.global_optimization = False
     pytest.seed = None
 
 def test_run_infer_dm(infer_dm_args):
-    dadi_cli.run_infer_dm(pytest)
+    dadi_cli.run_infer_dm(args)
 
+def test_run_infer_dm_misid(infer_dm_args):
+    pytest.nomisid = False
+    pytest.output_prefix = "tests/test_results/example.two_epoch.demo_misid.params"
+    pytest.p0 = [1, 0.5, 1e-2]
+    pytest.ubounds = [10, 10, 0.999]
+    pytest.lbounds = [1e-3, 1e-3, 1e-4]
+    dadi_cli.run_infer_dm(args)
+
+def test_run_infer_dm_bestfit(infer_dm_args):
+    pytest.bestfit_p0 = "tests/example_data/example.bestfit.two_epoch.demo.params.InferDM.opts.0"
+    pytest.output_prefix = "tests/test_results/example.two_epoch.demo_bestfit_p0.params"
+    dadi_cli.run_infer_dm(args)
+
+try:
+    import work_queue as wq
+    skip = False
+except:
+    skip = True
+
+@pytest.mark.skipif(skip, reason="Could not load Work Queue")
 def test_run_infer_dm_workqueue(infer_dm_args):
     import subprocess
     factory = subprocess.Popen(
-        "work_queue_factory -T local -M test-dm-two-epoch -P ./tests/mypwfile --workers-per-cycle=0 -t 10 --cores=1  -w 1 -W 1",
+        "work_queue_factory -T local -M test-dm-two-epoch -P ./tests/mypwfile --workers-per-cycle=0 -t 10 --cores=1  -w 1 -W 1 -E '--connection-mode by_apparent_ip'",
         shell=True,
     )
+    pytest.model = "two_epoch"
+    pytest.model_file = None
+    pytest.output_prefix = "tests/test_results/example.two_epoch.demo_wq.params"
     pytest.work_queue = ['test-dm-two-epoch', 'tests/mypwfile']
-    dadi_cli.run_infer_dm(pytest)
+    dadi_cli.run_infer_dm(args)
     factory.kill()
+
+
+    # Clean up args
+    pytest.nomisid = True
+    pytest.p0 = [1, 0.5]
+    pytest.ubounds = [10, 10]
+    pytest.lbounds = [1e-3, 1e-3]
+    pytest.work_queue = []
 
 
 def test_run_simulate_dm():
