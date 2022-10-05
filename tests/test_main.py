ec6b82b49122434dc26f699517bd577603c0f776
tjstruck <tjstruck@email.arizona.edu>
2022-10-04 18:44:48 -0700

    Add fixture for DFE tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -43,6 +43,45 @@ def infer_dm_args():
     pytest.global_optimization = False
     pytest.seed = None
 
+
+@pytest.fixture
+def infer_dfe_args():
+    pytest.fs_mix = "tests/example_data/split_mig_non_mix.fs"
+    pytest.fs_1d_lognorm = "tests/example_data/split_mig_non_1d.fs"
+    pytest.fs_2d_lognorm = "tests/example_data/split_mig_non_2d.fs"
+    pytest.model = "split_mig_fix_T"
+    pytest.model_file = "tests/example_data/example_models"
+    pytest.demo_popt = "tests/example_data/example.split_mig_fix_T.demo.params.InferDM.bestfits"
+    pytest.cache1d = "tests/example_data/cache_split_mig_1d.bpkl"
+    pytest.cache2d = "tests/example_data/cache_split_mig_2d.bpkl"
+    pytest.pdf1d = "lognormal"
+    pytest.pdf2d = "biv_lognormal"
+    pytest.mix_pdf = False
+    pytest.ratio = 2.31
+    pytest.pdf_file = False
+    pytest.p0 = [1, 1, 1]
+    pytest.grids = [120, 140, 160]
+    pytest.ubounds = [10, 10, 10]
+    pytest.lbounds = [1e-3, 1e-3, 1e-3]
+    pytest.constants = -1
+    pytest.nomisid = True
+    pytest.cuda = False
+    pytest.maxeval = 100
+    pytest.maxtime = 300
+    pytest.output_prefix = "tests/test_results/example.split_mig_fix_T.dfe.params"
+    pytest.check_convergence = True
+    pytest.force_convergence = False
+    pytest.work_queue = []
+    pytest.debug_wq = False
+    pytest.cpus = 1
+    pytest.gpus = 0
+    pytest.optimizations = 3
+    pytest.bestfit_p0 = None
+    pytest.delta_ll = 0.001
+    pytest.global_optimization = False
+    pytest.seed = None
+
+
 def test_run_infer_dm(infer_dm_args):
     dadi_cli.run_infer_dm(pytest)
 
@@ -99,5 +138,10 @@ def test_run_simulate_dm():
     simulate_args.inference_file = False
 
     dadi_cli.run_simulate_dm(simulate_args)
+
+
+
+
+
 
 
