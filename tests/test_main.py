ca10a20e4d5344113e66e063d9baaa3bef514de5
tjstruck <tjstruck@email.arizona.edu>
2022-10-18 09:56:40 -0700

    Add more inference tests and other misc tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 import dadi_cli.__main__ as dadi_cli
 import os
+import glob
 
 from dadi_cli.GenerateFs import *
 from dadi_cli.GenerateCache import *
@@ -16,6 +17,78 @@ try:
 except:
     pass
 
+def test_run_generate_fs():
+    def gen_fs_args():
+        return
+    gen_fs_args.vcf = "tests/example_data/1KG.YRI.CEU.biallelic.synonymous.snps.withanc.strict.short.vcf"
+    gen_fs_args.output = "tests/test_results/main_gen_data.fs"
+    gen_fs_args.bootstrap = None
+    gen_fs_args.chunk_size = None
+    gen_fs_args.seed = 1234
+    gen_fs_args.pop_ids = ["YRI", "CEU"]
+    gen_fs_args.pop_info = "examples/data/1KG.YRI.CEU.popfile.txt"
+    gen_fs_args.projections = [216, 198]
+    gen_fs_args.polarized = True
+    gen_fs_args.marginalize_pops = None
+    gen_fs_args.subsample = False
+    gen_fs_args.mask_shared = False
+    gen_fs_args.mask = False
+    dadi_cli.run_generate_fs(gen_fs_args)
+    gen_fs_args.mask = True
+    dadi_cli.run_generate_fs(gen_fs_args)
+    gen_fs_args.mask_shared = True
+    dadi_cli.run_generate_fs(gen_fs_args)
+    os.remove(gen_fs_args.output)
+
+
+def test_run_simulate_dm():
+    def simulate_args():
+        return
+    simulate_args.model = "two_epoch"
+    simulate_args.model_file = None
+    simulate_args.p0 = [1, 0.5]
+    simulate_args.ns = [10]
+    simulate_args.grids = [20, 30, 40]
+    simulate_args.misid = False
+    simulate_args.output = "tests/test_results/main_simulate_two_epoch.fs"
+    simulate_args.inference_file = False
+
+    dadi_cli.run_simulate_dm(simulate_args)
+    os.remove(simulate_args.output)
+
+def test_run_simulate_dfe():
+    def simulate_args():
+        return
+    simulate_args.p0 = [1, 1.5, 0, 0.01, 0.05]
+    simulate_args.cache1d = "tests/example_data/cache_split_mig_1d.bpkl"
+    simulate_args.cache2d = "tests/example_data/cache_split_mig_2d.bpkl"
+    simulate_args.pdf1d = "lognormal"
+    simulate_args.pdf2d = "biv_lognormal"
+    simulate_args.theta_ns = 2.31
+    simulate_args.misid = True
+    simulate_args.output = "tests/test_results/main_simulate_mix_dfe.fs"
+
+    dadi_cli.run_simulate_dfe(simulate_args)
+    os.remove(simulate_args.output)
+
+def test_run_generate_cache():
+    def generate_cache_args():
+        return
+    generate_cache_args.model = "split_mig_fix_T_one_s"
+    generate_cache_args.model_file = "tests/example_data/example_models"
+    generate_cache_args.grids = [20, 40, 60]
+    generate_cache_args.demo_popt = "tests/example_data/example.split_mig_fix_T.demo.params.InferDM.bestfits"
+    generate_cache_args.gamma_bounds = (1e-4, 10)
+    generate_cache_args.gamma_pts = 5
+    generate_cache_args.additional_gammas = []
+    generate_cache_args.output = "tests/test_results/test.cache.bpkl"
+    generate_cache_args.sample_sizes = [10, 10]
+    generate_cache_args.cpus = 1
+    generate_cache_args.gpus = 0
+    generate_cache_args.dimensionality = 1
+    dadi_cli.run_generate_cache(generate_cache_args)
+    os.remove(generate_cache_args.output)
+
 @pytest.fixture
 def infer_dm_args():
     pytest.fs = "tests/example_data/two_epoch_syn.fs"
@@ -28,8 +101,8 @@ def infer_dm_args():
     pytest.constants = -1
     pytest.nomisid = True
     pytest.cuda = False
-    pytest.maxeval = 100
-    pytest.maxtime = 300
+    pytest.maxeval = 10
+    pytest.maxtime = 10
     pytest.output_prefix = "tests/test_results/example.two_epoch.demo.params"
     pytest.check_convergence = True
     pytest.force_convergence = False
@@ -43,6 +116,48 @@ def infer_dm_args():
     pytest.global_optimization = False
     pytest.seed = None
 
+def test_run_infer_dm(infer_dm_args):
+    dadi_cli.run_infer_dm(pytest)
+    for ele in glob.glob(pytest.output_prefix+"*"):
+        os.remove(ele)
+
+def test_run_infer_no_custom_model_dm(infer_dm_args):
+    pytest.model = "three_epoch_inbreeding"
+    pytest.model_file = None
+    pytest.constants = [None, 1, None, None, None]
+    pytest.ubounds = [10, 10, 10, 10, 1]
+    pytest.lbounds = [1e-3, 1e-3, 1e-3, 1e-3, 1e-5]
+    pytest.p0 = -1
+    pytest.maxeval = False
+    pytest.inbreeding = True
+    pytest.global_optimization = True
+    dadi_cli.run_infer_dm(pytest)
+    for ele in glob.glob(pytest.output_prefix+"*"):
+        os.remove(ele)
+
+def test_run_infer_dm_misid(infer_dm_args):
+    pytest.nomisid = False
+    pytest.output_prefix = "tests/test_results/example.two_epoch.demo_misid.params"
+    pytest.p0 = [1, 0.5, 1e-2]
+    pytest.ubounds = [10, 10, 0.999]
+    pytest.lbounds = [1e-3, 1e-3, 1e-4]
+    dadi_cli.run_infer_dm(pytest)
+    for ele in glob.glob(pytest.output_prefix+"*"):
+        os.remove(ele)
+
+def test_run_infer_dm_global_bestfit(infer_dm_args):
+    pytest.global_optimization = True
+    print(pytest.nomisid)
+    pytest.bestfit_p0 = "tests/example_data/example.bestfit.two_epoch.demo.params.InferDM.opts.0"
+    pytest.output_prefix = "tests/test_results/example.two_epoch.demo_bestfit_p0.params"
+    pytest.nomisid = False
+    pytest.p0 = [1, 0.5, 1e-2]
+    pytest.ubounds = [10, 10, 0.999]
+    pytest.lbounds = [1e-3, 1e-3, 1e-4]
+    pytest.maxeval = 10
+    dadi_cli.run_infer_dm(pytest)
+    for ele in glob.glob(pytest.output_prefix+"*"):
+        os.remove(ele)
 
 @pytest.fixture
 def infer_dfe_args():
@@ -59,20 +174,18 @@ def infer_dfe_args():
     pytest.mix_pdf = False
     pytest.ratio = 2.31
     pytest.pdf_file = False
-    pytest.p0 = [1, 1, 1]
+    pytest.p0 = [1, 1]
     pytest.grids = [120, 140, 160]
-    pytest.ubounds = [10, 10, 10]
-    pytest.lbounds = [1e-3, 1e-3, 1e-3]
+    pytest.ubounds = [10, 10]
+    pytest.lbounds = [1e-3, 1e-3]
     pytest.constants = -1
     pytest.nomisid = True
     pytest.cuda = False
     pytest.maxeval = 100
     pytest.maxtime = 300
-    pytest.output_prefix = "tests/test_results/example.split_mig_fix_T.dfe.params"
+    pytest.output_prefix = "tests/test_results/example.split_mig_fix_T."
     pytest.check_convergence = True
     pytest.force_convergence = False
-    pytest.work_queue = []
-    pytest.debug_wq = False
     pytest.cpus = 1
     pytest.gpus = 0
     pytest.optimizations = 3
@@ -80,28 +193,62 @@ def infer_dfe_args():
     pytest.delta_ll = 0.001
     pytest.global_optimization = False
     pytest.seed = None
+    pytest.work_queue = []
+    pytest.debug_wq = False
+    pytest.port = 9123
 
+def test_run_infer_dfe_1d(infer_dfe_args):
+    pytest.fs = pytest.fs_1d_lognorm
+    pytest.pdf2d = None
+    pytest.cache2d = None
+    pytest.mix_pdf = None
+    pytest.output_prefix += "1d_lognormal_dfe"
+    dadi_cli.run_infer_dfe(pytest)
+    for ele in glob.glob(pytest.output_prefix+"*"):
+        os.remove(ele)
 
-def test_run_infer_dm(infer_dm_args):
-    dadi_cli.run_infer_dm(pytest)
+def test_run_infer_dfe_1d_gamma(infer_dfe_args):
+    pytest.fs = pytest.fs_1d_lognorm
+    pytest.pdf1d = "gamma"
+    pytest.pdf2d = None
+    pytest.cache2d = None
+    pytest.mix_pdf = None
+    pytest.output_prefix += "1d_gamma_dfe"
+    dadi_cli.run_infer_dfe(pytest)
+    for ele in glob.glob(pytest.output_prefix+"*"):
+        os.remove(ele)
 
-def test_run_infer_dm_misid(infer_dm_args):
-    pytest.nomisid = False
-    pytest.output_prefix = "tests/test_results/example.two_epoch.demo_misid.params"
-    pytest.p0 = [1, 0.5, 1e-2]
+def test_run_infer_dfe_2d_lognormal(infer_dfe_args):
+    pytest.fs = pytest.fs_2d_lognorm
+    pytest.pdf1d = None
+    pytest.cache1d = None
+    pytest.mix_pdf = None
+    pytest.output_prefix += "2d_lognormal_dfe"
+    pytest.p0 = [1, 1, 0.5]
     pytest.ubounds = [10, 10, 0.999]
-    pytest.lbounds = [1e-3, 1e-3, 1e-4]
-    dadi_cli.run_infer_dm(pytest)
+    pytest.lbounds = [1e-3, 1e-3, 1e-3]
+    dadi_cli.run_infer_dfe(pytest)
+    for ele in glob.glob(pytest.output_prefix+"*"):
+        os.remove(ele)
 
-def test_run_infer_dm_bestfit(infer_dm_args):
-    print(pytest.nomisid)
-    pytest.bestfit_p0 = "tests/example_data/example.bestfit.two_epoch.demo.params.InferDM.opts.0"
-    pytest.output_prefix = "tests/test_results/example.two_epoch.demo_bestfit_p0.params"
-    pytest.nomisid = False
-    pytest.p0 = [1, 0.5, 1e-2]
-    pytest.ubounds = [10, 10, 0.999]
-    pytest.lbounds = [1e-3, 1e-3, 1e-4]
-    dadi_cli.run_infer_dm(pytest)
+def test_run_infer_dfe_mix(infer_dfe_args):
+    pytest.fs = pytest.fs_2d_lognorm
+    pytest.mix_pdf = 'mixture_lognormal'
+    pytest.output_prefix += "mix_lognormal_dfe"
+    pytest.p0 = [1, 1, 0, 0.5]
+    pytest.ubounds = [10, 10, None, 0.999]
+    pytest.lbounds = [1e-3, 1e-3, None, 1e-3]
+    pytest.constants = [None, None, 0, None]
+    dadi_cli.run_infer_dfe(pytest)
+    fids = glob.glob(pytest.output_prefix+"*")
+    print(fids)
+    opt = open(fids[-1],'r').readlines()
+    print(opt)
+    fix_check = [float(ele.split('\t')[3]) == 0.0 for ele in opt[2:]]
+    for ele in fix_check:
+        assert(ele)
+    for fi in fids:
+        os.remove(fi)
 
 try:
     import work_queue as wq
@@ -112,7 +259,7 @@ except:
 if os.path.exists("/home/runner/work/dadi-cli/dadi-cli"):
     skip = True
 
-@pytest.mark.skipif(skip, reason="Could not load Work Queue or in GitAction environments")
+# @pytest.mark.skipif(skip, reason="Could not load Work Queue or in GitAction environments")
 # @pytest.mark.skip(reason="Issues running Work Queue right now")
 def test_run_infer_dm_workqueue(infer_dm_args):
     import subprocess
@@ -124,20 +271,9 @@ def test_run_infer_dm_workqueue(infer_dm_args):
     pytest.work_queue = ['pytest-dadi-cli', 'tests/mypwfile']
     dadi_cli.run_infer_dm(pytest)
     factory.kill()
+    for ele in glob.glob(pytest.output_prefix+"*"):
+        os.remove(ele)
 
-def test_run_simulate_dm():
-    def simulate_args():
-        return
-    simulate_args.model = "two_epoch"
-    simulate_args.model_file = None
-    simulate_args.p0 = [1, 0.5]
-    simulate_args.ns = [10]
-    simulate_args.grids = [20, 30, 40]
-    simulate_args.misid = False
-    simulate_args.output = "tests/test_results/main_simulate_two_epoch.fs"
-    simulate_args.inference_file = False
-
-    dadi_cli.run_simulate_dm(simulate_args)
 
 
 
