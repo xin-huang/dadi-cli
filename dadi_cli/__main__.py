9d00681f6082cb97d93f4c4d1437d4d9e0548a0f
tjstruck <tjstruck@email.arizona.edu>
2022-11-03 12:53:05 -0700

    Fix weird bug from merging with master on GitHub

--- dadi_cli/__main__.py
+++ dadi_cli/__main__.py
@@ -108,7 +108,7 @@ def run_simulate_dfe(args):
         cache2d,
         args.pdf1d,
         args.pdf2d,
-        args.theta_ns,
+        args.ratio,
         args.misid,
         args.output,
     )
